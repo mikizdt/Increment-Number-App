4ca5ec0a64c0eb156c546bbe8e9ee805302d994f
Michelangelo Calori <70917863+mikizdt@users.noreply.github.com>
2024-08-12 21:18:55 -0400

    Increase Number App

---
+++ .bash_history
@@ -0,0 +1,19 @@
+ls
+cd Messaging App/
+cd Messaging ćlearApp/
+clear
+ls
+cd Messaging\ App/
+ls
+touch app.js
+ls
+start
+clear
+ls
+echo "# Messaging-App" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mikizdt/Messaging-App.git
+git push -u origin main
---
+++ IncrementeNumberApp/index.js
@@ -0,0 +1,31 @@
+// Start the counter at 0;
+let count = 0;
+
+// Funtionality for the buttons
+const increaseBtn = document.getElementById("increaseBtn");
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+increaseBtn.onclick = function () {
+  count++;
+  h1.textContent = count;
+};
+
+decreaseBtn.onclick = function () {
+  count--;
+  // Cannot decrease lower then 0
+  if (count < 0) {
+    count = 0;
+  }
+
+  h1.textContent = count;
+};
+
+resetBtn.onclick = function () {
+  h1.textContent = count = 0;
+};
+
+// Target and change the content to display the number
+let h1 = document.querySelector("h1");
+
+h1.textContent = count;
